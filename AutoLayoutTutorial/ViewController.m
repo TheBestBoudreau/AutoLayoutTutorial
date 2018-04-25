ff54a390bc6b961f223bfb9b60c53570f10ffa60
Tyler Boudreau <boudreaut@hotmail.com>
2018-04-24 21:28:50 -0700

    Finished

---
+++ AutoLayoutTutorial/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  AutoLayoutTutorial
+//
+//  Created by Tyler Boudreau on 2018-04-24.
+//  Copyright © 2018 Tyler Boudreau. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonTapped:(UIButton *)sender
+{
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
+}
+
+@end
